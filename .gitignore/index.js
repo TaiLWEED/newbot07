751ceb9030ff2cb7a511b69de0df4499db3192f8
TaiLWEED <50691853+TaiLWEED@users.noreply.github.com>
2019-05-16 20:11:43 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,114 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N Bonjour') {
+    msg.reply('bonjour bienvenu(e) sur notre serveur!');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N ca va?') {
+    msg.reply('ca va tres bien et toi?');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N ça va ?') {
+    msg.reply('ca va tres bien et toi?');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N tg') {
+    msg.reply('veuillez faire preuve d un peux plus de maturité merci :)');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N bonjour') {
+    msg.reply('bonjour bienvenu(e) sur notre serveur!');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N tg') {
+    msg.reply('vas pénétré t as soeur consangin de merde! cordialement');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N je t aime') {
+    msg.reply('désolé je ne recherche pas de relation serieuse si tu veux les sites pornographiques existe pour ton qu´a désespéré ;)');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N tu fais quoi ?') {
+    msg.reply('je suis en train de parlé a un moulot et toi?');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N tfk?') {
+    msg.reply('je suis en train de parlé a un moulot et toi?');
+  }
+});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N on fait un enfant toxico ?') {
+    msg.reply('Ho oui! je reverais d´avoir un enfants comme toi');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'N help') {
+    msg.reply('MESSAGE :   "N on fait un enfant toxico" <Reponse> / "N tfk?" <Reponse> / "N tu fais quoi ?" <Reponse> / "N je t aime" <Reponse> / "N tg" <Reponse> / "N bonjour" <Reponse> / "N tg" <Reponse> / "N ça va ?" <Reponse> / "N ca va?" <Reponse> "N Bonjour" <Reponse>');
+
+  }
+});
+
+client.login("NTc3NTgyNDEyMjc2NTYzOTY5.XNnJlQ.BsT6RXv8vhXpsllBML7nkK6NyaI");
